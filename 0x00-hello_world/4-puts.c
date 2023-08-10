7b2c5f6b42aa59b215c0e92cd8f5c627afd63e6a
Jmmadubuike <josephmmadubuike@yahoo.com>
2023-08-10 15:50:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
-i#include<stdio.h>
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
