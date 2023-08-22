aad7fe0cd96f414918b36c94d97e3b215a629a6c
Jmmadubuike <josephmmadubuike@yahoo.com>
2023-08-22 01:03:40 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character.
+ * puts2 - Prints every other character of a string.
  * @str: Pointer to a string.
  */
 void puts2(char *str)
@@ -11,7 +11,7 @@ void puts2(char *str)
 	while (str[i] != '\0')
 	{
 	_putchar(str[i]);
-	i += 2; /* Move to the next even-indexed character */
+	i += 2;
 	}
 
 	_putchar('\n');
