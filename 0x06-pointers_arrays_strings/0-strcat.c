cc2ca5de6faea3ed7faaea31b5a5a4e536e92049
Jmmadubuike <josephmmadubuike@yahoo.com>
2023-08-24 16:03:57 -0700

    wahala

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,33 @@
-ii#include "main.h"
+#include "main.h"
 
 /**
- * _strncat - Concatenate two strings using at most n bytes from src.
+ * _strcat - Concatenates two strings.
  * @dest: Destination string.
  * @src: Source string.
  *
  * Return: A pointer to the resulting string dest.
  */
-
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
+	int src_len = 0;
 	int i = 0;
 
 	while (dest[dest_len] != '\0')
 	{
-		dest_len++;
+	dest_len++;
+	}
+	while (src[src_len] != '\0')
+	{
+	src_len++;
 	}
 
-	while (src[i] != '\0')
+	while (i < src_len)
 	{
 	dest[dest_len + i] = src[i];
 	i++;
 	}
-
-
 	dest[dest_len + i] = '\0';
+
 	return (dest);
 }
