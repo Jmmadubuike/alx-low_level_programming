e5e1463a0f9873f0afa88950f6ec759305f06f0a
Jmmadubuike <josephmmadubuike@yahoo.com>
2023-08-24 18:20:20 -0700

    kwechiri

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdbool.h>
+#include <ctype.h>
 
 /**
  * cap_string - Capitalizes all words of a string.
@@ -10,26 +12,26 @@ char *cap_string(char *str)
 {
 	bool new_word = true;
 
-
 	for (int i = 0; str[i] != '\0'; i++)
-	{
+{
 	if (new_word && islower(str[i]))
 	{
-		str[i] = toupper(str[i]);
-		new_word = false;
+	str[i] = toupper(str[i]);
+	new_word = false;
 	}
-		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+	else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
 		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		 str[i] == '}')
+	{
+	new_word = true;
 	}
-		new_word = true;
-	}
-		else
+	else
 	{
 		new_word = false;
 	}
-	}
+}
 
 	return (str);
 }
